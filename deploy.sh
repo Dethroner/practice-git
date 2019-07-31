0ceb7d7dc2cf2056a4a4e988cd4f5edba5bc3f15
Detroner <dethroner995@gmail.com>
2019-07-31 08:59:06 +0300

    Edit 2 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && /usr/local/rvm/gems/ruby-2.4.1/bin/bundle install
-puma -d
+/usr/local/rvm/gems/ruby-2.4.1/bin/puma -d
 ps aux | grep puma
