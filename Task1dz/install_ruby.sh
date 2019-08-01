2c4568c2d6ea817c3dea7356e4df868d6670fd3f
Detroner <dethroner995@gmail.com>
2019-08-01 15:01:20 +0300

    Edit 4 install_ruby.sh

--- Task1dz/install_ruby.sh
+++ Task1dz/install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | /bin/bash -s stable
+/usr/bin/gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+/usr/bin/curl -sSL https://get.rvm.io | /bin/bash -s stable
 source /etc/profile.d/rvm.sh
 /usr/local/rvm/bin/rvm requirements
 /usr/local/rvm/bin/rvm install 2.4.1
