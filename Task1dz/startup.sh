4a65d43b6649fc5746cb3cef77860947d2048f0c
Detroner <dethroner995@gmail.com>
2019-08-02 09:11:05 +0300

    Update startup.sh

--- Task1dz/startup.sh
+++ Task1dz/startup.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-wget https://github.com/Dethroner/practice-git/blob/master/Task1dz/Deploy.tar
-tar -xvf Deploy.tar
-cd Deploy
-bash install_ruby.sh
-bash install_mongodb.sh
-bash deploy.sh
+#! /bin/bash
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get --assume-yes update
+apt-get --assume-yes install mongodb-org
+systemctl start mongod
+systemctl enable mongod
+apt-get --assume-yes update
+apt-get --assume-yes install ruby-full ruby-bundler buil-essential
+cd "$HOME" || exit
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
