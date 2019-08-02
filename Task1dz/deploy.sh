1073a0fd59d91016cc936f8fdf070c0500e017d2
Detroner <dethroner995@gmail.com>
2019-08-02 09:26:19 +0300

    Update

--- Task1dz/deploy.sh
+++ Task1dz/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/Artemmkin/reddit.git
-cd reddit && /usr/local/rvm/gems/ruby-2.4.1/bin/bundle install
-/usr/local/rvm/gems/ruby-2.4.1/bin/puma -d
+cd reddit && bundle install
+puma -d
 ps aux | grep puma
