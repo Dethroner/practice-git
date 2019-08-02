6f4760f3b544c82ff42d8aa682ec5ed1ca3e5565
Detroner <dethroner995@gmail.com>
2019-08-02 12:21:47 +0300

    Update 1 startup.sh

--- Task1dz/startup.sh
+++ Task1dz/startup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get --assume-yes update
 apt-get --assume-yes install mongodb-org
