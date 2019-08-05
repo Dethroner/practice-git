435576a6b46833ce257becb2508523d9f16df36c
Detroner <dethroner995@gmail.com>
2019-08-05 12:52:57 +0300

    Up #9 README.md & add create-reddit-vm.sh

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#create reddit vm
+gcloud compute instances create reddit-app\
+  --boot-disk-size=10GB \
+  --image-family reddit-full \
+  --machine-type=g1-small \
+  --tags puma-server \
+  --restart-on-failure
+  
