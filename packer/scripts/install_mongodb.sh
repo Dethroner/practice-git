80f4b88a652c3ee090c27d6eb93e32c84d1bb1fa
Detroner <dethroner995@gmail.com>
2019-08-06 14:58:47 +0300

    Add packer task

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get -y update
+apt-get -y install mongodb-org
+systemctl start mongod
+systemctl enable mongod
