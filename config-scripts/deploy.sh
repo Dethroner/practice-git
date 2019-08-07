06d6abe09cd9fc8c74dde8b63cfd7fb1211e7cdf
Detroner <dethroner995@gmail.com>
2019-08-07 09:51:49 +0300

    Fix rep

--- config-scripts/deploy.sh
+++ config-scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo -i
 cd "$HOME" || exit
 git clone -b monolith https://github.com/express42/reddit.git && cd reddit && bundle install && puma -d
--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo -i
 apt-get -y  update
 apt-get -y install ruby-full ruby-bundler build-essential
