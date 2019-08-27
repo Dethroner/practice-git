f2ee95fe57ca876098f22697c8c9337c81a51286
Detroner <dethroner995@gmail.com>
2019-08-27 14:52:43 +0300

    Add vagrant to docker

---
+++ vagrant/examples/2/files/docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##################################################
+# Install Ansible                                #
+# Author by Dethroner, 2019                      #
+##################################################
+
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
