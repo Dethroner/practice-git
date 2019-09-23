57a90b0706cb14af57f12ec1b2961f1ec9f9fe70
Detroner <dethroner995@gmail.com>
2019-09-23 12:27:12 +0300

    Add vagrant + gitlab ci

---
+++ vagrant/examples/4/files/gitlab.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##################################################
+# Install Gitlab CI                              #
+# Author by Dethroner, 2019                      #
+##################################################
+
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo apt-get -y install python-pip
+sudo pip install docker-compose 
+
+sudo mkdir -p /srv/gitlab/config /srv/gitlab/data /srv/gitlab/logs
+sudo chmod -R 777 /srv/gitlab
